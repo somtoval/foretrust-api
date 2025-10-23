cf10c244c310c58e3415de5302d3aa4c60d23e0d
somtoval <ogbesomto042@gmail.com>
2025-10-23 06:25:56 +0100

    handled cors by stating allow methods explicity

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
